eb7157ef920f97e7a5985182c84795e2c511de75
rian carraro <riancarraro@gmail.com>
2026-03-23 15:30:24 -0300

    commit

--- js/supabase.js
+++ js/supabase.js
@@ -7,8 +7,8 @@
 // 4. Copie a "Project URL" e a "anon public key"
 // 5. Cole abaixo substituindo os valores
 
-const SUPABASE_URL = 'https://SEU_PROJECT_ID.supabase.co';
-const SUPABASE_KEY = 'SUA_ANON_KEY_AQUI';
+const SUPABASE_URL = 'https://ngvxhpcdonxriwmjahtt.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5ndnhocGNkb254cml3bWphaHR0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQyODg4MjIsImV4cCI6MjA4OTg2NDgyMn0.CnTu8U4muYo84XD26_mK5rfJPiEAZ82ETSQ0uX5vRBs';
 
 const { createClient } = supabase;
 const db = createClient(SUPABASE_URL, SUPABASE_KEY);
